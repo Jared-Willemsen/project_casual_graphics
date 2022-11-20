bc3aa4f6010e4cb7131de980fddfad204caf58a0
Shayaan <shayaanelahi@gmail.com>
2022-11-20 16:13:37 +0100

    New files and comments
    Added photos of the fractals to be used in the fractal view for the buttons, as well as comments for readibility

--- fractal.py
+++ fractal.py
@@ -1,8 +1,23 @@
+#ttk is a submodule allowing for newer widgets in Tk version 8.5
 from tkinter import *
-import matplotlib as plt
-import numpy as np
+from tkinter import ttk
+import os
+from os import listdir
 
+#initialization of the window/GUI
 mainwindow = Tk()
+mainwindow.title("Fractal Art")
+mainwindow.attributes("-fullscreen", True)
+
+#Goes through all the fractal pictures and puts them as images on the buttons
+folder = "fractal_pictures"
+for picture_path in os.listdir(folder):
+    fractal_button = Button(mainwindow)
+    img = PhotoImage(file=f"fractal_pictures/{picture_path}")
+    resized = img.subsample(7, 7)
+    fractal_button.config(image=resized)
+    fractal_button.pack()
+
 mainwindow.mainloop()
 
 
