669cd156bd457489be492d89f594b1d848c10db2
Shayaan <shayaanelahi@gmail.com>
2022-11-22 14:47:07 +0100

    Added Canvas

--- fractal.py
+++ fractal.py
@@ -9,11 +9,16 @@ mainwindow.attributes("-fullscreen", True)
 mainwindow.configure(background="#FFD700")
 
 #frame that will include all the widgets/UI
-fractal_frame = Frame(mainwindow, height=1000, width=1000, bg="#333333")
+fractal_frame = Frame(mainwindow, highlightbackground="#ADD8E6", 
+                    highlightthickness=20, height=1000, width=1000, bg="#333333")
 fractal_frame.place(relwidth=0.8,relheight=0.8, rely=0.1, relx=0.1)
-frac_label = Label(fractal_frame, text="FRACTAL ART", 
+frac_label = Label(fractal_frame, text="FRACTAL MUSEUM", 
             font=("Verdana italic",60), bg="#333333", fg="#f00")
 frac_label.pack()
+
+#canvas that will be inside the frame for the created image
+fractal_canvas = Canvas(fractal_frame, bg="white", height=400, width=500)
+fractal_canvas.pack()
 
 #Goes through all the fractal pictures and puts them as images on the buttons
 fractal_folder = "fractal_pictures"
@@ -29,6 +34,7 @@ for picture_path in os.listdir(fractal_folder):
     else:
         continue
 
+#goes for the 
 frame_folder = "picture_frames"
 t = 100
 for picture_path in os.listdir(frame_folder):
@@ -41,9 +47,6 @@ for picture_path in os.listdir(frame_folder):
         t+=200
     else:
         continue
-
-
-
 
 mainwindow.mainloop()
 
